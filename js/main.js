d2dd09a1bc40a10d329e7f294826ae22e2407cd4
Shy Choudhury <shayer76@gmail.com>
2017-05-31 18:59:55 +0100

    Convert to Sass

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+// FEWD Final Project
+
+
+/*
+PSEUDO-CODE
+------------------------------
+When a nav button is clicked 
+  - check if the nav menu is already seen
+  - if the nav can be seen then hide the nav bar
+  - else show the nav bar
+
+
+*/
+
+$(document).ready( function(){
 console.log('it works');
+// when a nav button is clicked 
+
+});
